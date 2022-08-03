dd170ad9828fd2566a1e2d28a513816939f44830
tereza <terezamorodev@gmail.com>
2022-08-03 07:34:14 -0300

    Added js full script +  minor changes to HTML + CSS changes

--- eas.js
+++ eas.js
@@ -1,19 +1,63 @@
-const canvaDiv = document.getElementById('canva');
-const slider = document.getElementById('sizeSelector');
+    const canvaDiv = document.getElementById('canva');
+    const slider = document.getElementById('slider');
+    const pageBody = document.getElementsByTagName('body');
+    let canvaSize = 16;
+    let choosenColor = 'black';
+    let choosenMode = 'normal';
 
-let canvaSize = 16;
-
-function createCanva(size){
-
-    let canvaCell;
-    for(let i = 0; i < size*size; i++){
-        canvaCell = document.createElement('div');
-        canvaCell.className = 'cell';
-        document.getElementById('canva').appendChild(canvaCell);
+    //erases all cells from canva div
+    function deleteCells(){
+        canvaDiv.innerHTML = '';
     }
-    document.getElementById('canva').style.gridTemplate= `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
-}
 
-slider.onchange = (e) =>{
-    createCanva(e.target.value);
-}
+    //changes cell's color according to mode
+    function changeColor(cell){
+        if(choosenMode === 'normal'){
+            cell.target.style.backgroundColor = choosenColor;
+        }
+        else if(choosenMode === 'rainbow'){
+            let randomColor = Math.floor(Math.random()*16777215).toString(16);
+            cell.target.style.backgroundColor = '#' + randomColor;
+        }
+        else{
+            cell.target.style.backgroundColor = 'white';
+        }
+    }
+
+
+    function changeMode(mode){
+        choosenMode = mode;
+    }
+
+    //function creates grid of canvaSize and appends to canvaDiv
+    function createCanva(){
+
+        let canvaCell;
+        deleteCells();
+        for(let i = 0; i < canvaSize*canvaSize; i++){
+            canvaCell = document.createElement('div');
+            canvaCell.className = 'cell';
+            canvaCell.addEventListener('mouseover', changeColor);
+            document.getElementById('canva').appendChild(canvaCell);
+        }
+        document.getElementById('canva').style.gridTemplate= `repeat(${canvaSize}, 1fr) / repeat(${canvaSize}, 1fr)`;
+    }
+
+    let rainbowButton = document.getElementById('rainbowBtn');
+    rainbowButton.onclick = () => changeMode('rainbow');
+
+    let eraserButton = document.getElementById('eraserBtn');
+    eraserButton.onclick = () => changeMode('eraser');
+
+    let colorSelector = document.getElementById('colorSelector');
+    colorSelector.onchange = (e) =>{
+        changeMode('normal');
+        choosenColor = e.target.value;
+    }
+
+    slider.onchange = (e) =>{
+        canvaSize = e.target.value;
+        createCanva();
+    }
+
+    window.addEventListener("load", createCanva);
